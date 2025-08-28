b7d0d32d111aa63afe27ee44dcc9f5dd1adda083
SijmeJan <14568532+SijmeJan@users.noreply.github.com>
2025-08-28 10:39:38 +0200

    add MPI support

--- examples/scalar_advection.py
+++ examples/scalar_advection.py
@@ -17,15 +17,15 @@ def initial_conditions(coords):
     x = coords[0]
 
     U = np.zeros((n_eq, len(coords[0])))
-    #U[0,:] = np.exp(-np.log(2)*(x+0.7)**2/0.0009)*(x > -0.8)*(x < -0.6) +\
-    #    1.0*(x > -0.4)*(x < -0.2) +\
-    #    (1 - np.abs(10*(x - 0.1)))*(x > 0.0)*(x < 0.2) +\
-    #    np.sqrt(np.abs(1 - 100*(x - 0.5)**2))*(x > 0.4)*(x < 0.6)
-    U[0,:] = np.sin(np.pi*coords[0])
+    U[0,:] = np.exp(-np.log(2)*(x+0.7)**2/0.0009)*(x > -0.8)*(x < -0.6) +\
+        1.0*(x > -0.4)*(x < -0.2) +\
+        (1 - np.abs(10*(x - 0.1)))*(x > 0.0)*(x < 0.2) +\
+        np.sqrt(np.abs(1 - 100*(x - 0.5)**2))*(x > 0.4)*(x < 0.6)
+    #U[0,:] = np.sin(np.pi*coords[0])
 
     return U
 
-from boxset.output.basic import *
+from boxset.output.parallel import *
 from boxset.coords import create_coordinates
 
 def visualise(direc, save_index):
@@ -35,26 +35,31 @@ def visualise(direc, save_index):
     config = configparser.ConfigParser()
     config.read(direc + 'scalar_advection.ini')
 
-    n_ghost=4
-    t, U = restore_from_dump(save_index, config['Output']['direc'])
+    n_ghost = np.int32(config['Grid']['n_ghost'])
 
-    coords = create_coordinates(config)
+    coords, pos, global_dims = create_coordinates(config)
     x = coords[0]
 
-    #plt.plot(x[n_ghost:-n_ghost], U[0,n_ghost:-n_ghost])
+    state = 0*initial_conditions(coords)
+
+    t, U = restore_from_dump(state, save_index, config['Output']['direc'], pos, global_dims, n_ghost)
+
+    plt.plot(x[n_ghost:-n_ghost], U[0,n_ghost:-n_ghost])
 
     plt.xlabel('x')
 
-    t, U0 = restore_from_dump(0, config['Output']['direc'])
-    plt.plot(x[n_ghost:-n_ghost], U[0,n_ghost:-n_ghost] - U0[0,n_ghost:-n_ghost])
-    #plt.plot(x[n_ghost:-n_ghost], U0[0,n_ghost:-n_ghost])
+    print(np.shape(U))
 
-    print('L1 norm: ', np.mean(np.abs(U[0,n_ghost:-n_ghost] + U0[0,n_ghost:-n_ghost])))
-    print('L2 norm: ', np.sqrt(np.mean((U[0,n_ghost:-n_ghost] + U0[0,n_ghost:-n_ghost])**2)))
+    t, U0 = restore_from_dump(state.copy(), 0, config['Output']['direc'], pos, global_dims, n_ghost)
+    #plt.plot(x[n_ghost:-n_ghost], U[0,n_ghost:-n_ghost] - U0[0,n_ghost:-n_ghost])
+    plt.plot(x[n_ghost:-n_ghost], U0[0,n_ghost:-n_ghost])
+
+    print('L1 norm: ', np.mean(np.abs(U[0,n_ghost:-n_ghost] - U0[0,n_ghost:-n_ghost])))
+    print('L2 norm: ', np.sqrt(np.mean((U[0,n_ghost:-n_ghost] - U0[0,n_ghost:-n_ghost])**2)))
     plt.show()
 
 from boxset.simulation import simulation
 
-simulation("/Users/sjp/Desktop/boxset/scalar_advection.ini", initial_conditions, set_boundary, restore_index=-1)
+#simulation("/Users/sjp/Desktop/boxset/scalar_advection.ini", initial_conditions, set_boundary, restore_index=-1)
 
 visualise('/Users/sjp/Desktop/boxset/', 1)
