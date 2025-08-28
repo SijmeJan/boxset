fe7040c232c8d9652dd62ef3e2105f03cc3596c3
SijmeJan <14568532+SijmeJan@users.noreply.github.com>
2025-08-28 10:36:52 +0200

    add MPI support

--- boxset/output/basic.py
+++ boxset/output/basic.py
@@ -1,6 +1,7 @@
 import numpy as np
+from mpi4py import MPI
 
-def save_dump(time, state, save_index, save_path):
+def save_dump(time, state, save_index, save_path, pos, global_dims, n_ghost):
     '''Save current time and state in npz file'''
     t = np.asarray([time])
 
@@ -9,7 +10,7 @@ def save_dump(time, state, save_index, save_path):
 
     return
 
-def restore_from_dump(restore_index, restore_path):
+def restore_from_dump(state, restore_index, restore_path, pos, global_dims, n_ghost):
     '''Restore simulation time and state from npz file'''
 
     filename = restore_path + 'dump{}.npz'.format(restore_index)
