3d30c770056f1dd4c5f717bff3bc46f5ab966c00
SijmeJan <14568532+SijmeJan@users.noreply.github.com>
2025-08-28 10:40:15 +0200

    add MPI support + uniform noise

--- examples/streaming_instability.py
+++ examples/streaming_instability.py
@@ -27,6 +27,10 @@ def initial_conditions(coords):
     pressure_parameter = 0.05
     mu = 0.2
     stokes = 1.0
+    noise_level = 0.01
+
+    h = 0.5*noise_level
+    l = -0.5*noise_level
 
     rng = np.random.default_rng(seed=1)
 
@@ -37,19 +41,19 @@ def initial_conditions(coords):
     U = np.zeros((n_eq, len(coords[0]), len(coords[1])))
     U[0,:,:] = 1.0
     U[1,:,:] = 2*pressure_parameter*J1*denom + \
-      rng.uniform(low=-0.005, high=0.005, size=np.shape(U[1,:,:]))
+      rng.uniform(low=l, high=h, size=np.shape(U[1,:,:]))
     U[2,:,:] = -pressure_parameter*(1 + J0)*denom + \
-      rng.uniform(low=-0.005, high=0.005, size=np.shape(U[1,:,:]))
-    U[3,:,:] = rng.uniform(low=-0.005, high=0.005, size=np.shape(U[1,:,:]))
+      rng.uniform(low=l, high=h, size=np.shape(U[1,:,:]))
+    U[3,:,:] = rng.uniform(low=l, high=h, size=np.shape(U[1,:,:]))
 
     U[4,:,:] = mu
-    U[5,:,:] = 2*pressure_parameter*(J1 - stokes*(1 + J0))*denom/(1 + stokes**2) + rng.uniform(low=-0.005, high=0.005, size=np.shape(U[1,:,:]))
-    U[6,:,:] = -pressure_parameter*(1 + J0 + stokes*J1)*denom/(1 + stokes**2)+ rng.uniform(low=-0.005, high=0.005, size=np.shape(U[1,:,:]))
-    U[7,:,:] = rng.uniform(low=-0.005, high=0.005, size=np.shape(U[1,:,:]))
+    U[5,:,:] = 2*pressure_parameter*(J1 - stokes*(1 + J0))*denom/(1 + stokes**2) + rng.uniform(low=l, high=h, size=np.shape(U[1,:,:]))
+    U[6,:,:] = -pressure_parameter*(1 + J0 + stokes*J1)*denom/(1 + stokes**2)+ rng.uniform(low=l, high=h, size=np.shape(U[1,:,:]))
+    U[7,:,:] = rng.uniform(low=l, high=h, size=np.shape(U[1,:,:]))
 
     return U
 
-from boxset.output.basic import *
+from boxset.output.parallel import *
 from boxset.coords import create_coordinates
 
 def visualise(direc, save_index):
@@ -63,14 +67,20 @@ def visualise(direc, save_index):
 
     n_ghost = np.int32(config['Grid']['n_ghost'])
 
-    t, U = restore_from_dump(save_index, config['Output']['direc'])
-
-    coords = create_coordinates(config)
+    coords, pos, global_dims = create_coordinates(config)
     x = coords[0]
     y = coords[1]
 
-    cf = plt.contourf(x[n_ghost:-n_ghost], y[n_ghost:-n_ghost], (np.transpose(U[0,n_ghost:-n_ghost,n_ghost:-n_ghost])),
-                     levels=100, cmap='terrain')
+    state = initial_conditions(coords)
+
+    t, U = restore_from_dump(state, save_index, config['Output']['direc'], pos, global_dims, n_ghost)
+
+    f = U[4,n_ghost:-n_ghost,n_ghost:-n_ghost]
+    #levels = np.linspace(np.log10(0.02), np.log10(2), 100)
+
+    cf = plt.contourf(x[n_ghost:-n_ghost], y[n_ghost:-n_ghost],
+                      np.transpose((f)),
+                      levels=100, cmap='plasma')
 
     plt.xlabel('x')
     plt.ylabel('z')
@@ -78,8 +88,9 @@ def visualise(direc, save_index):
 
     plt.show()
 
+
 from boxset.simulation import simulation
 
 simulation("/Users/sjp/Desktop/boxset/streaming_instability.ini", initial_conditions, set_boundary, restore_index=-1)
 
-visualise('/Users/sjp/Desktop/boxset/', 8)
+visualise('/Users/sjp/Desktop/boxset/', 100)
