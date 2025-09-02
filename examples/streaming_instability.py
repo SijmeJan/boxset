0cc055ddad0466a6080264b534c7d46d8677848f
SijmeJan <14568532+SijmeJan@users.noreply.github.com>
2025-09-02 09:46:23 +0200

    cleanup

--- examples/streaming_instability.py
+++ examples/streaming_instability.py
@@ -92,7 +92,7 @@ def visualise(ini_file, save_index):
 
 from boxset.simulation import simulation
 
-#simulation("/Users/sjp/Desktop/boxset/streaming_instability.ini", initial_conditions, set_boundary, restore_index=-1)
+#simulation("/Users/sjp/Desktop/boxset/streaming_instability.ini", initial_conditions, set_boundary, restore_index=1)
 #simulation("/home/sijmejanpaarde/streaming_instability.ini", initial_conditions, set_boundary, restore_index=-1)
 
-visualise("/Users/sjp/Desktop/boxset/streaming_instability.ini", 100)
+visualise("/Users/sjp/Desktop/boxset/streaming_instability.ini", 10)
