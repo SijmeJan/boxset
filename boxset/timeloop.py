606ec37a75f42aba71f2c43f3f023c27b0e09b7a
SijmeJan <14568532+SijmeJan@users.noreply.github.com>
2025-09-06 20:21:06 +0200

    add safety_factor

--- boxset/timeloop.py
+++ boxset/timeloop.py
@@ -4,7 +4,7 @@ from mpi4py import MPI
 from .timesteppers.rk3 import time_stepper, rk_cfl
 from .rhs import calculate_rhs, calc_time_step
 
-def timeloop(state, coords, start_time, end_time, courant_number, n_ghost, boundary_conditions, cpu_grid):
+def timeloop(state, coords, start_time, end_time, courant_number, n_ghost, boundary_conditions, cpu_grid, safety_factor):
     '''
     Evolve the state from start_time to end_time. Returns final state.
     '''
@@ -26,7 +26,7 @@ def timeloop(state, coords, start_time, end_time, courant_number, n_ghost, bound
             dt = end_time - t
 
         # Do one time step
-        state = time_stepper(state, coords, dt, calculate_rhs, n_ghost, boundary_conditions, cpu_grid)
+        state = time_stepper(state, coords, dt, calculate_rhs, n_ghost, boundary_conditions, cpu_grid, safety_factor)
 
         if MPI.COMM_WORLD.Get_rank() == 0:
             print('t = ', t, 'dt = ', dt)
