9746c478797e1091c28c49d964fa4e740463a77e
SijmeJan <14568532+SijmeJan@users.noreply.github.com>
2025-09-06 20:21:17 +0200

    add safety_factor

--- boxset/timesteppers/rk2.py
+++ boxset/timesteppers/rk2.py
@@ -1,8 +1,8 @@
 rk_cfl = 1.0
 
-def time_stepper(U, coords, dt, rhs, n_ghost, boundary_conditions, cpu_grid):
+def time_stepper(U, coords, dt, rhs, n_ghost, boundary_conditions, cpu_grid, safety_factor):
     '''
     SSP RK2 integrator
     '''
-    U1 = U + dt*rhs(U, coords, n_ghost, boundary_conditions, cpu_grid, dt)
-    return 0.5*U + 0.5*U1 + 0.5*dt*rhs(U1, coords, n_ghost, boundary_conditions, cpu_grid, dt)
+    U1 = U + dt*rhs(U, coords, n_ghost, boundary_conditions, cpu_grid, safety_factor*dt)
+    return 0.5*U + 0.5*U1 + 0.5*dt*rhs(U1, coords, n_ghost, boundary_conditions, cpu_grid, safety_factor*dt)
